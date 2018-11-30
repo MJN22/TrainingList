f5889ed515cc7366a0a9ec8bcdb7829cc682cf03
axelt18 <tovar_axel@yahoo.com>
2018-11-30 10:07:45 -0600

    added simple functionality to the library class

--- MyListLibrary/MyListLibrary/MemoryListLibrary.cs
+++ MyListLibrary/MyListLibrary/MemoryListLibrary.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace MyListLibrary
+namespace MemoryListLibrary.Library
 {
-    public class MemoryListLibrary
+    public class MemoryListLibrary<T> where T : new()
     {
-    
+        public List<T> _list = new List<T>();
+
+        public void Add(T item) {
+        //Add generic type item to the list
+            _list.Add(item);
+          
+        }
+
+        public void Remove(T item) {
+        //removes generic type item from the list
+
+        if(Contains(item)) {
+            _list.Remove(item);
+
+           } else 
+               Console.WriteLine("Item is not in list");
+
+        } 
+        public bool Contains(T item)
+         // checks to see if the item already exists in the list
+        {
+            return _list.Contains(item);
+        }
 
     }
 }
